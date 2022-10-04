b076b33c1d3ae80f0dfe83bb30ff3272ad78e24b
samcoswymysl <rafalkrukowskipraca123@gmail.com>
2022-10-04 16:32:11 +0200

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ArticleModule } from './article/article.module';
+import { AnnouncementModule } from './announcement/announcement.module';
+import { MailModule } from './mail/mail.module';
+import { CommentModule } from './comment/comment.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [ AuthModule, ArticleModule, AnnouncementModule, MailModule, CommentModule, UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
